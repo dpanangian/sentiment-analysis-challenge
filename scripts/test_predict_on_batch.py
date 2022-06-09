0f06717c40cd6c90ab2fb1792d9a8f50ba087d13
Daniel Panangian <dan.sibarani@gmail.com>
2022-06-09 21:10:49 +0200

    fox test

--- scripts/test_predict_on_batch.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'http://127.0.0.1:8000/predict'
-payload = {"text": "This app is so good!"}
-headers = {"accept: application/json","Content-Type":"application/json","access_token": "932ea2a0-29b9-44ea-8f08-6c42f761b205"}
-resp = requests.request("POST",url=url, json=payload, headers=headers)
-print(resp.json())
