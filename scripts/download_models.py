41bd2ec263a5828b6270f3961035d7b9b368be1e
Daniel Panangian <dan.sibarani@gmail.com>
2022-06-09 21:59:48 +0200

    fix download_models script and update dockerfile

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -1,3 +1,4 @@
 import gdown
-
+from pathlib import Path
+Path("models/").mkdir(parents=True, exist_ok=True)
 gdown.download_folder("https://drive.google.com/drive/folders/1PvOClskengpMakFUyQFqKELb4ipSpOtL?usp=sharing", output="models/",quiet=False, use_cookies=False)
